737f0ad6c2f56719d0dd057e18820f8b95310b94
tmiklu <61796331+tmiklu@users.noreply.github.com>
2024-05-16 19:57:39 +0200

    Add comment

--- comment.sh
+++ comment.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
 
+# push new comment, to do something
 if [ $# -eq 1 ] ; then
   ID=$(curl -s -u "xxx:xxx" -X GET https://jira.xxx.net/rest/api/2/issue/$1/comment/ | jq .comments[].id -r)
   while read line ; do curl -s -u 'xxx:xxx' -X DELETE "https://jira.xxx.net/rest/api/2/issue/${1}/comment/${line}" ; done <<< $ID
